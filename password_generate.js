5a229dd2e6d303173af507ad3f507404ebda8e8c
Doni <donisgemilang@gmail.com>
2021-12-31 01:31:09 +0700

    konsep

--- password_generate.js
+++ password_generate.js
@@ -7,28 +7,46 @@ let number = "1234567890";
 let options = [upperCase, lowerCase, number];
 let randomOptions = rand(options);
 let random = rand(randomOptions);
-console.log(rand(upperCase), rand(lowerCase), rand(number));
-console.log(random);
+// console.log(rand(upperCase), rand(lowerCase), rand(number));
+// console.log(randomOptions);
+// console.log(random)
 
 /*
 random generate done
 let hasil
 for loop
-jika hasil random sama dengan jenis yang sebelumnya maka random ulang
-jika tidak maka hasil push / +=
+jika hasil random tidak sama dengan jenis yang sebelumnya maka +=
+jika sama maka random ulang
 return hasil
 */
-let hasil = "";
-console.log(hasil);
+let hasil = "b";
+//console.log(hasil);
+let i = hasil.length -1
 //need fix
 let koreksi = () => {
+  let korek = 1
   for (let index = 0; index < randomOptions.length; index++) {
-    if (random == randomOptions[index]) {
-      console.log(true);
-      console.log(random, randomOptions, randomOptions[index]);
-      break;
-    } else {
-      console.log(false);
+    if ( hasil[i] === randomOptions[index]) {
+      // console.log(true);
+      // console.log(random, randomOptions, randomOptions[index])
+      korek -= 1
+      //console.log('benar')
     }
   }
+  if(korek){
+    hasil += random
+  }
+  return hasil
 };
+
+// if(hasil == lowerCase){
+//   console.log(true)
+// }
+// else{
+//   console.log(false)
+// }
+
+for (let index = 0; index < 5; index++) {
+  koreksi()
+}
+console.log(hasil)
