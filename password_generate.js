c12e9e7299c4f16cabfe4a806ee7a432f1d7e98f
Doni <donisgemilang@gmail.com>
2021-12-30 19:59:33 +0700

    konsep

--- password_generate.js
+++ password_generate.js
@@ -1,15 +1,14 @@
-function rand(x){
-  return x[Math.floor(Math.random() * x.length)]
+function rand(x) {
+  return x[Math.floor(Math.random() * x.length)];
 }
-let upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYXZ'
-let lowerCase = 'abcdefghijklmnopqrstuvwxyz'
-let number = '1234567890'
-let options = [upperCase,lowerCase,number]
-let randomOptions = rand(options)
-let random = rand(randomOptions)
-console.log(rand(upperCase),rand(lowerCase),rand(number))
-console.log(random)
-
+let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYXZ";
+let lowerCase = "abcdefghijklmnopqrstuvwxyz";
+let number = "1234567890";
+let options = [upperCase, lowerCase, number];
+let randomOptions = rand(options);
+let random = rand(randomOptions);
+console.log(rand(upperCase), rand(lowerCase), rand(number));
+console.log(random);
 
 /*
 random generate done
@@ -19,14 +18,17 @@ jika hasil random sama dengan jenis yang sebelumnya maka random ulang
 jika tidak maka hasil push / +=
 return hasil
 */
-let hasil = ''
-console.log(hasil)
+let hasil = "";
+console.log(hasil);
 //need fix
-for (let index = 0; index < randomOptions.length; index++) {
-if(random == randomOptions[index]){
-  console.log(true)
-  console.log(random, randomOptions, randomOptions[index])
-  break
-}
-else{console.log(false)}
-}
+let koreksi = () => {
+  for (let index = 0; index < randomOptions.length; index++) {
+    if (random == randomOptions[index]) {
+      console.log(true);
+      console.log(random, randomOptions, randomOptions[index]);
+      break;
+    } else {
+      console.log(false);
+    }
+  }
+};
