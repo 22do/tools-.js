2a5ed8a39b37e4c3358ef35c39b5299fe2209667
Doni <donisgemilang@gmail.com>
2021-12-30 17:48:11 +0700

    konsep

---
+++ password_generate.js
@@ -0,0 +1,17 @@
+function rand(x){
+  return x[Math.floor(Math.random() * x.length)]
+}
+let upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYXZ'
+let lowerCase = 'abcdefghijklmnopqrstuvwxyz'
+let number = '1234567890'
+let options = [upperCase,lowerCase,number]
+let randomOptions = rand(options)
+console.log(rand(upperCase),rand(lowerCase),rand(number))
+console.log(rand(randomOptions))
+
+/*
+random generate done
+for loop
+jika hasil random sama dengan jenis yang sebelumnya maka random ulang
+jika tidak maka push / +=
+*/
